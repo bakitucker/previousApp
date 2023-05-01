7365adc2ade56bb2265a043572736598a4945882
bakitucker <bakitucker8@gmail.com>
2023-05-01 01:40:02 -0400

    had the wrong files in the folder

--- public/main.js
+++ public/main.js
@@ -1,33 +1,16 @@
-var makeFav = document.getElementsByClassName("fa-solid fa-heart");
-var unFav = document.getElementsByClassName("fa-regular fa-heart");
+var checkPalindrome = document.getElementsByClassName("check");
 var trash = document.getElementsByClassName("fa-trash");
 
-
-
-
-
-
-
-
-
-
-
-Array.from(makeFav).forEach(function(element) {
+Array.from(checkPalindrome).forEach(function(element) {
       element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const num = this.parentNode.parentNode.childNodes[3].innerText
-        const makeFav = this.parentNode.parentNode.childNodes[9].innerText
-        const address = this.parentNode.parentNode.childNodes[5].innerText
-        const email = this.parentNode.parentNode.childNodes[7].innerText
-        fetch('addresses', {
+        const word = this.parentNode.parentNode.childNodes[1].innerText
+        const checkP = this.parentNode.parentNode.childNodes[3].innerText
+        fetch('words', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
-            'name': name,
-            'number': num,
-            'makeFav': makeFav,
-            'address': address,
-            'email': email
+            'word': word,
+            'checkP': checkP,
           })
         })
         .then(response => {
@@ -40,48 +23,20 @@ Array.from(makeFav).forEach(function(element) {
       });
 });
 
-Array.from(unFav).forEach(function(element) {
-  element.addEventListener('click', function(){
-    const name = this.parentNode.parentNode.childNodes[1].innerText
-    const num = this.parentNode.parentNode.childNodes[3].innerText
-    const makeFav = this.parentNode.parentNode.childNodes[9].innerText
-    const address = this.parentNode.parentNode.childNodes[5].innerText
-    const email = this.parentNode.parentNode.childNodes[7].innerText
-    fetch('addresses/unFav', {
-      method: 'put',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        'name': name,
-        'number': num,
-        'makeFav': makeFav,
-        'address': address,
-        'email': email
-      })
-    })
-    .then(response => {
-      if (response.ok) return response.json()
-    })
-    .then(data => {
-      console.log(data)
-      window.location.reload(true)
-    })
-  });
-});
-
 
 
 Array.from(trash).forEach(function(element) {
       element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const num = this.parentNode.parentNode.childNodes[3].innerText
-        fetch('addresses', {
+        const word = this.parentNode.parentNode.childNodes[1].innerText
+        const checkP = this.parentNode.parentNode.childNodes[3].innerText
+        fetch('words', {
           method: 'delete',
           headers: {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify({
-            'name': name,
-            'number': num
+            'word': word,
+            'checkP': checkP
           })
         }).then(function (response) {
           window.location.reload()
--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const MongoClient = require('mongodb').MongoClient
 
 var db, collection;
 
-const url = "mongodb+srv://bakitucker8:VIwTdREZ9k0yLJIC@bakitucker1.gvx0z4s.mongodb.net/?retryWrites=true&w=majority";
-const dbName = "personalOne";
+const url = "mongodb+srv://bakitucker8:baki123@previousapp.msj0cu4.mongodb.net/?retryWrites=true&w=majority";
+const dbName = "previousApp";
 
 app.listen(1010, () => {
     MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
@@ -24,39 +24,25 @@ app.use(bodyParser.json())
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  db.collection('addresses').find().toArray((err, result) => {
+  db.collection('words').find().toArray((err, result) => {
     if (err) return console.log(err)
     res.render('index.ejs', {messages: result})
   })
 })
 
-app.post('/messages', (req, res) => {
-  db.collection('addresses').insertOne({name: req.body.name, number: req.body.number, makeFav:"",address: req.body.address, email: req.body.email}, (err, result) => {
+app.post('/words', (req, res) => {
+  let checkTheP = req.body.word === req.body.word.split("").reverse().join("") ? "It is a palindrome!" : "I'm sorry. It is not a palindrome!"
+  db.collection('words').insertOne({word: req.body.word, checkP:checkTheP, }, (err, result) => {
     if (err) return console.log(err)
     console.log('saved to database')
     res.redirect('/')
   })
 })
 
-app.put('/addresses', (req, res) => {
-  db.collection('addresses')
-  .findOneAndUpdate({name: req.body.name, number: req.body.number}, {
+app.put('/words', (req, res) => {
+  db.collection('words')
+  .findOneAndUpdate({word: req.body.word}, {
     $set: {
-      makeFav:'★★'
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
-})
-app.put('/addresses/unFav', (req, res) => {
-  db.collection('addresses')
-  .findOneAndUpdate({name: req.body.name, number: req.body.number}, {
-    $set: {
-      makeFav:''
     }
   }, {
     sort: {_id: -1},
@@ -70,8 +56,8 @@ app.put('/addresses/unFav', (req, res) => {
 
 
 
-app.delete('/addresses', (req, res) => {
-  db.collection('addresses').findOneAndDelete({name: req.body.name, number: req.body.number}, (err, result) => {
+app.delete('/words', (req, res) => {
+  db.collection('words').findOneAndDelete({word: req.body.word}, (err, result) => {
     
     if (err) return res.send(500, err)
 
